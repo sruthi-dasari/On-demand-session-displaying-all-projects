519a01c2042f60597c4aa3ed329e27549b2d4c0d
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-12-05 11:46:16 +0530

    Errors coming in output

--- src/components/TabItem/index.js
+++ src/components/TabItem/index.js
@@ -1,8 +1,10 @@
 import './index.css'
 
 const TabItem = props => {
-  const {tabDetails, updateActiveTabId} = props
+  const {tabDetails, updateActiveTabId, isActive} = props
   const {displayText, tabId} = tabDetails
+
+  const activeTabClassName = isActive ? 'active-tab-btn' : ''
 
   const onClickTabItem = () => {
     updateActiveTabId(tabId)
@@ -10,7 +12,11 @@ const TabItem = props => {
 
   return (
     <li className="tab-item-container ">
-      <button type="button" className="tab-btn" onClick={onClickTabItem}>
+      <button
+        type="button"
+        className={'tab-btn ${activeTabClassName}'}
+        onClick={onClickTabItem}
+      >
         {displayText}
       </button>
     </li>
